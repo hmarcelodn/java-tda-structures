47c0d78f7c1065cddff32086a7c0ea89e529ce76
Hugo Marcelo Del Negro <hmdelnegro@gmail.com>
2017-07-06 02:09:27 -0300

    Impl List Adjacent

--- src/impl/GrafoLA.java
+++ src/impl/GrafoLA.java
@@ -17,44 +17,121 @@ public class GrafoLA implements GrafoTDA{
 		NodoArista sigArista;
 	}
 	
+	NodoGrafo origen;
+	
 	public void inicializarGrafo() {
-		// TODO Auto-generated method stub
-		
+		origen = null;
 	}
 
 	public void agregarVertice(int v) {
-		// TODO Auto-generated method stub
-		
+		NodoGrafo aux = new NodoGrafo();
+		aux.nodo = v;
+		aux.arista = null;
+		aux.sigNodo = origen;
+		origen = aux;
 	}
 
 	public void eliminarVertice(int v) {
-		// TODO Auto-generated method stub
+		if(origen.nodo== v){
+			origen = origen.sigNodo;
+		}
+		
+		NodoGrafo aux = origen;
+		
+		while(aux != null){
+			
+			// Elimina Aristas que van hacia v
+			this.eliminarAristaNodo(aux, v);
+			
+			// Elimina Nodo
+			if(aux.sigNodo != null && aux.sigNodo.nodo == v){
+				aux.sigNodo = aux.sigNodo.sigNodo;
+			}
+			
+			aux = aux.sigNodo;
+		}
 		
 	}
 
 	public ConjuntoTDA vertices() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ConjuntoTDA conjuntoVertices = new ConjuntoLD();
+		conjuntoVertices.inicializarConjunto();
+		
+		NodoGrafo aux = origen;
+		
+		while(aux != null){
+			conjuntoVertices.agregar(aux.nodo);
+			aux = aux.sigNodo;
+		}
+		
+		return conjuntoVertices;
 	}
 
 	public void agregarArista(int v1, int v2, int peso) {
-		// TODO Auto-generated method stub
-		
+		NodoGrafo auxOrigen = vert2Nodo(v1);
+		NodoGrafo auxDestino = vert2Nodo(v2);
+			
+		NodoArista aristaAux = new NodoArista();
+		aristaAux.etiqueta = peso;
+		aristaAux.nodoDestino = auxDestino;
+		auxOrigen.arista = aristaAux;		
 	}
-
+	
 	public void eliminarArista(int v1, int v2) {
-		// TODO Auto-generated method stub
-		
+		NodoGrafo auxOrigen = this.vert2Nodo(v1);		
+		this.eliminarAristaNodo(auxOrigen, v2);
 	}
 
 	public boolean existeArista(int v1, int v2) {
-		// TODO Auto-generated method stub
-		return false;
+		NodoArista arista = destino2Arista(v1, v2);
+		
+		return (arista != null);
 	}
 
 	public int pesoArista(int v1, int v2) {
-		// TODO Auto-generated method stub
-		return 0;
+		NodoArista arista = destino2Arista(v1, v2);
+		
+		return arista.etiqueta;
+	}
+	
+	private void eliminarAristaNodo(NodoGrafo nodo, int v){
+		NodoArista auxArista = nodo.arista;
+		
+		if(auxArista.nodoDestino.nodo == v){
+			nodo.arista = auxArista.sigArista;
+		}
+		else{
+			while(auxArista.sigArista != null && auxArista.sigArista.nodoDestino.nodo != v){
+				auxArista = auxArista.sigArista.sigArista;
+			}				
+			
+			if(auxArista != null){
+				auxArista.sigArista = auxArista.sigArista.sigArista;
+			}
+		}	
+	}
+	
+	private NodoGrafo vert2Nodo(int n){
+		NodoGrafo aux = origen;
+		
+		while(aux != null && aux.nodo != n){
+			aux = aux.sigNodo;
+		}		
+		
+		return aux;
+	}
+
+	
+	private NodoArista destino2Arista(int v1, int v2){
+		NodoGrafo auxNodo = this.vert2Nodo(v1);
+		NodoArista auxArista = auxNodo.arista;
+		
+		while(auxArista != null && auxArista.nodoDestino.nodo != v2){
+			auxArista = auxArista.sigArista;
+		}
+		
+		return auxArista;
 	}
 
 }
